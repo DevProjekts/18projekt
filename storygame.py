2293480a1ad1b6e4f31c461199038ac81dbd0ff5
DevProjekts <88111594+DevProjekts@users.noreply.github.com>
2021-09-02 19:44:48 +0200

    Create storygame.py

---
+++ storygame.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from pag import GameWorld
+from pag import CommandLineInterface
+from pag import classes
+
+# This gives the Game the list of all locations that is updated every time a
+# new location is created. Since setting a variable to another variable with a
+# list points to the one list's memory address, the list in the game class also
+# updates.
+gameworld = GameWorld(locations=classes.location_list)
+cli = CommandLineInterface(gameworld)
+
+class ToiletPaper(classes.Item):
+    def __init__(self):
+        super().__init__(name='toilet paper',
+                         description='The toilet paper is labeled "X-t'
+                         'raSoft.',
+                         loc_description='A roll of toilet paper is in '
+                         'the room.',
+                         weight=1)
+
+
+home = classes.Location('Home', start=True, show_name_when_exit=True)
+home.description = 'You\'re at home.'
+bathroom = classes.Location('Bathroom', items=[ToiletPaper()], show_name_when_exit=True)
+bathroom.description = 'You\'re in the bathroom.'
+home.exits = {'south': bathroom}
+bathroom.exits = {'north': home}
+
+cli.play()
